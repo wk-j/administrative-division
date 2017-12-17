5f6c3d4485dfdb50631d68ba355e587d6f77b0fd
wk-j <somnuk.wk@gmail.com>
2017-12-17 16:55:47 +0700

    Update tests

--- src/ThailandAdministrativeDivision.Tests/Tests.cs
+++ src/ThailandAdministrativeDivision.Tests/Tests.cs
@@ -5,24 +5,26 @@ using System.Linq;
 namespace ThailandAdministrativeDivision.Tests {
     public class Tests {
         [Fact]
-        public void ThereAre77Changwats() {
+        public void Changwats() {
             var division = Division.Load();
             var count = division.Changwats.Count();
             Assert.Equal(77, count);
         }
 
         [Fact]
-        public void ThereAre878Amphoes() {
+        public void Amphoes() {
             var division = Division.Load();
             var count = division.Ampoes.Count();
-            // Assert.Equal(878, count);
+            Assert.Equal(928, count);
+            // Expected: 928
+            // Actual:   904
         }
 
         [Fact]
-        public void ThereAre7255Tombons() {
+        public void Tambons() {
             var division = Division.Load();
             var count = division.Tambons.Count();
-            // Assert.Equal(7255, count);
+             Assert.Equal(7364, count);
         }
 
         [Fact]
